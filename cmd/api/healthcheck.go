456289f39a0f8ce43d46e82eb43945253d9242a0
Milen Todev <todevcode@gmail.com>
2025-07-09 11:09:07 +0300

    feat: add structured json response error messages

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -15,8 +15,6 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 
 	err := app.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
-		app.logger.Error(err.Error())
-		http.Error(w, "The server encoutered a problem and could not process your request", http.StatusInternalServerError)
-		return
+		app.serverErrorResponse(w, r, err)
 	}
 }
